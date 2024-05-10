172ff13f6984f3edb2b54855d6c2840ca17f21fc
15anil-sak <shiramshettyanil@gmail.com>
2024-05-10 22:15:24 +0530

    Add files via upload
    my first function for creating classes and objects

---
+++ class.py
@@ -0,0 +1,7 @@
+class person:
+    def _init_(self,a=ravi,b=male):
+        self.name=a
+        self.gender=b
+    def showInfo(self):
+        print('name:',self.name)
+        print('gender',self.gender)
